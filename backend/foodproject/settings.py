e56f70ebcd79855f4091d6860ea6c440eb82ccf0
David Velasquez <40569893+davidve0206@users.noreply.github.com>
2023-12-19 22:13:47 -0500

    Fix a misconfiguration in settings

--- backend/foodproject/settings.py
+++ backend/foodproject/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["54.85.150.87"]
 
@@ -140,8 +140,8 @@ if os.getenv('USE_S3') == 'TRUE':
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
     # s3 static settings
-    STATIC_LOCATION = 'static'
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
+    AWS_LOCATION = 'static'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     # s3 media settings
     PUBLIC_MEDIA_LOCATION = 'media'
